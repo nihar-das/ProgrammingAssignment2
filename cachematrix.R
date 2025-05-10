d1de6c15a68087d67869d9b288a6731c54805766
nihardas <cipherxo1@protonmail.com>
2025-05-10 13:45:50 +0530

    completed assignment matrix inverse caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions helps to avoid wasteful computation of inverse
+## of matrix if done already by using cached result
 
-## Write a short comment describing this function
+
+## This function takes a matrix and returns a special type of 
+## cacheMatrix (i.e list) with operations set and get for matrix
+## setMatInverse and getInverse for inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matInverse <- NULL
+  
+  set <- function(m) {
+    if(!identical(x, m)) {
+      x <<- m
+      matInverse <<- NULL
+    }else {
+      message("Matrix not updated. Old and new matrix are same")
+    }
+  }
+  
+  get <- function()
+    x
+  
+  setMatInverse <- function(m) {
+    dim_m <- dim(m)
+    dim_x <- dim(x)
+    
+    if (identical(dim_x, dim_m)) {
+      matInverse <<- m
+    } else {
+      message("Dimension mismatch between given matrix and inverse")
+    }
+  }
+  
+  getMatInverse <- function()
+    matInverse
+  
+  list(
+    set = set,
+    get = get,
+    setMatInverse = setMatInverse,
+    getMatInverse = getMatInverse
+  )
 }
 
 
-## Write a short comment describing this function
+## This function take special cacheMatrix and return inverse
+## from cache if exist already or else computes inverse and save to cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matInverse <- x$getMatInverse()
+  
+  if(!is.null(matInverse)){
+    message("Using cached data")
+    return(matInverse)
+  }
+  
+  mat <- x$get()
+  matInverse <- solve(mat, ...)
+  x$setMatInverse(matInverse)
+  
+  matInverse
 }
